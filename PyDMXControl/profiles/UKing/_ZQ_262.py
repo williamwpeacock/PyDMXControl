b1ee6d1f0ce76d32ccffbebd2cae5ee16eb71b73
will <william.wpeacock@gmail.com>
2024-12-11 23:07:01 +0000

    added UKing ZQ_262 profile

---
+++ PyDMXControl/profiles/UKing/_ZQ_262.py
@@ -0,0 +1,26 @@
+"""
+ *  PyDMXControl: A Python 3 module to control DMX using OpenDMX or uDMX.
+ *                Featuring fixture profiles, built-in effects and a web control panel.
+ *  <https://github.com/MattIPv4/PyDMXControl/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+"""
+
+from ..defaults import Fixture
+
+
+class ZQ_262(Fixture):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._register_channel('dimmer')
+        self._register_channel_aliases('dimmer', 'dim', 'd')
+        self._register_channel('red')
+        self._register_channel_aliases('red', 'r')
+        self._register_channel('green')
+        self._register_channel_aliases('green', 'g')
+        self._register_channel('blue')
+        self._register_channel_aliases('blue', 'b')
+        self._register_channel('strobe')
+        self._register_channel('mode')
+        self._register_channel('mode_control')
