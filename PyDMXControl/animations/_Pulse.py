076841ade34207f623e629fa93c168660c70f4e7
will <william.wpeacock@gmail.com>
2025-01-13 17:30:59 +0000

    fixed pulse animation

--- PyDMXControl/animations/_Pulse.py
+++ PyDMXControl/animations/_Pulse.py
@@ -4,20 +4,28 @@ from .. import Colors
 
 class Pulse(Animation):
 
-    def __init__(self, color, fixtures, mid_time, end_time, start_transform = Colors.mix, end_transform = Colors.mix, *args, **kwargs):
+    def __init__(self, fixture, color, mid_time, end_time, start_transform = Colors.mix, end_transform = Colors.mix, *args, **kwargs):
         # Pulse(color=Colors.Red, fixtures=[1], mid_time, end_time, start_transform, end_transform)
+        assert mid_time < end_time
         self.color = color
         self.mid_time = mid_time
         self.end_time = end_time
         self.start_transform = start_transform
         self.end_transform = end_transform
 
-        super().__init__(fixtures, end_time)
+        super().__init__(fixture, end_time, *args, **kwargs)
 
     def callback(self, now: float):
+        now = self.local_pos(now)
         if now <= self.mid_time:
-            new_color = self.start_transform(Colors.Black, self.color, now / self.mid_time)
+            new_color = self.start_transform(self.color, Colors.Black, now / self.mid_time)
         elif now > self.mid_time:
-            new_color = self.end_transform(self.color, Colors.Black, now / (self.end_time - self.mid_time))
+            progress = (now - self.mid_time) / (self.end_time - self.mid_time)
+            new_color = self.end_transform(self.color, Colors.Black, 1-progress)
 
-        self.fixtures.color(new_color)
+        self.fixture.color(new_color)
+
+    def stop(self):
+        self.fixture.color(Colors.Black)
+        super().stop()
+
